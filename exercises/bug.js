c4726970f71a834e4f60b64e5d82b390dbbe0b1c
Miguel Fernandez <miguelferlez@gmail.com>
2025-01-28 12:49:21 +0100

    feat: Ejercicio 2 completado

---
+++ exercises/bug.js
@@ -0,0 +1,15 @@
+const calcularPromedio = (numeros) => {
+    let sumaTotal = 0;
+
+    // la condición del bucle era incorrecta, i no debería ser igual que numeros.length, pues numeros[5] no existe en listaNumeros y devolverá undefined.
+    for (let i = 0; i < numeros.length; i++) {
+        sumaTotal += numeros[i];
+    }
+
+    const promedio = sumaTotal / numeros.length;
+    return promedio;
+};
+
+const listaNumeros = [1, 2, 3, 4, 5];
+const promedioNumeros = calcularPromedio(listaNumeros);
+console.log(promedioNumeros);
